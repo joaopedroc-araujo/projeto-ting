f8cdd9a47c098efdf90fc4f07db5a58035f95c29
João <costaaraujojoaopedro@gmail.com>
2023-12-05 14:57:25 -0300

    FEAT: implementa file_management

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import os
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not os.path.exists(path_file):
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return
+
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+        return
+
+    with open(path_file, "r") as file:
+        lines = file.read().split("\n")
+        return lines
