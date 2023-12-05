69f96012c7773f3dd66bdec9d5640c43601d6d5c
João <costaaraujojoaopedro@gmail.com>
2023-12-05 15:13:47 -0300

    FEAT: implementa process de file_process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for item in list(instance._data):
+        if item["nome_do_arquivo"] == path_file:
+            return
+
+    lines = txt_importer(path_file)
+    if lines is None:
+        return
+
+    file_data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines,
+    }
+
+    instance.enqueue(file_data)
+    print(file_data)
 
 
 def remove(instance):
