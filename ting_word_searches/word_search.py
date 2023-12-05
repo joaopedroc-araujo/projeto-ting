e21bfbfe0e8e27e88d502583d24b60b33d95826f
João <costaaraujojoaopedro@gmail.com>
2023-12-05 16:21:55 -0300

    FEAT: implementa exists_word em word_search

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,24 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = list()
+    word = word.lower()
+
+    for index in range(len(instance)):
+        file_data = instance.search(index)
+        occurrences = []
+        for line_index, line in enumerate(file_data["linhas_do_arquivo"]):
+            if word in line.lower():
+                occurrences.append({"linha": line_index + 1})
+
+        if occurrences:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": file_data["nome_do_arquivo"],
+                    "ocorrencias": occurrences,
+                }
+            )
+
+    return result
 
 
 def search_by_word(word, instance):
